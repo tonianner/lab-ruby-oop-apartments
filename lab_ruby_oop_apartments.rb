03af3d14fe79f239baf906f73b4a6b31caca7b89
Toni Anne Romero <tonianne.romero@gmail.com>
2015-10-20 19:27:28 +0800

    still working on it

--- lab_ruby_oop_apartments.rb
+++ lab_ruby_oop_apartments.rb
@@ -1,46 +1,21 @@
-# class Building
-#   attr_accessor :address
-#   # attr_reader :appartments
+class Building
+  attr_accessor :address
 
-#   def initializer (address)
-#     @address = address
-#   end
-
-#   def add_appartnment(new_address)
-#     @address = new_address
-#     ::Appartment << @address #add index
-#   end
-
-#   def remove_apparment(index)
-#     @appartments.delete(index) #index of @appartment
-#   end
-
-# end
-
-# #-----------------------
-
-class Appartment
-  attr_accessor :number, :rent, :square_foot, :num_bedroom, :num_bathroom, :all_tenants
-
-  def initialize(number, rent, square_foot, num_bedroom, num_bathroom)
-    @number = number
-    @rent = rent
-    @square_foot = square_foot
-    @num_bedroom = num_bedroom
-    @num_bathroom = num_bathroom
+  def initializer (address)
+    @address = address
   end
 
-  def appartment_info
-    puts "Number: #{number}"
-    puts"Rent: $ #{rent}"
-    puts"Square foot: #{square_foot} "
-    puts "Bedrooms: #{num_bedroom}"
-    puts "Bathrooms: #{num_bathroom}"
+  def add_appartnment(new_address)
+    @address = new_address
+    ::Appartment << @address #add index
   end
+
+  def remove_apparment(index)
+    @appartments.delete(index) #index of @appartment
+  end
+
 end
 
-appartment1 = Appartment.new(1,2400,800,2,1)
-appartment1.appartment_info
 
 #-----------------------
 
@@ -55,15 +30,15 @@ class Tenant
 
   def rating(value)
     if value >= 760
-       "Credit rating: Excellent!"
+       "Excellent"
     elsif value >= 725
-      "Credit rating: Great!"
+      "Great"
     elsif value >= 660
-      "Credit rating: Good!"
+      "Good"
     elsif value >= 560
-      "Credit rating: Mediocre!"
+      "Mediocre"
     else
-      "Credit rating: bad!"
+      "Bad"
     end
   end
 
@@ -74,7 +49,53 @@ class Tenant
   end
 end
 
-tenant1 = Tenant.new("Toni",24, 900).profile
-tenant1.profile
+
+#-----------------------
+
+class Appartment
+  attr_accessor :number, :rent, :square_foot, :num_bedroom, :num_bathroom
+  attr_reader :tenants
+
+  def initialize(number, rent, square_foot, num_bedroom, num_bathroom, tenants)
+    @number = number
+    @rent = rent
+    @square_foot = square_foot
+    @num_bedroom = num_bedroom
+    @num_bathroom = num_bathroom
+    @tenants = []
+  end
+
+  def add_tenant(tenants)
+    if (tenant.credit_score != "Bad") && (@tenants.size +1) < @num_bedroom
+      @tenants.push tenant
+    else
+      raise "tenant #{tenant.name} has a bad score or not enough rooms"
+    end
+  end
+
+  def remove_all_tenants
+    @tenants.delete_if{ true }
+  end
+
+  def average_score
+    @tenants.inject{|sum, t| sum + t.credit_score}.to_f / @tenants.size
+  end
+
+  def remove_tenant(tenant)
+    name = tenant.class == Tenant ? tenant.name : tenant1
+    if not @tenants.delete_if {|t|t.name == name}
+      raise "appartment not found"
+    end
+  end
+end
+
+tenant1 = Tenant.new("Toni",24, 900)
+tenant2 = Tenant.new("Anne",89, 900)
 
 
+appt1 = Appartment.new(1,2400,800,2,1,4)
+appt2 = Appartment.new(2,3400,1200,3,2,5)
+
+# appt1.add_tenant(tenant1)
+
+puts appt1.tenants
